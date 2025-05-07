9728729d4cf26ad4f294967e9ec4aa089b0e4150
Chen,Hong-Jun <B1229049@cgu.edu.tw>
2025-05-07 14:37:36 +0800

    lab13

---
+++ lab13/react_login/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import CGU_Login from './SignInSide';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {/* {CGU_Login} */}
+        <CGU_Login />
+      </div>
+    </div>
+  );
+}
+
+export default App;
